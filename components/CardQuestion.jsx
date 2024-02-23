7c250fb080a2dad5128f4e74fa2ba3e9a32d2f8f
MaryFBakeer <mariabefrank@gmail.com>
2024-02-23 15:42:07 +0300

    rrr

--- components/CardQuestion.jsx
+++ components/CardQuestion.jsx
@@ -4,17 +4,20 @@ function CardQuestion({ question }) {
   // console.log(question);
   return (
     <div>
-      <div>
+      <div className="questimg">
         <p>{question.question}</p>
         <img className="imgQuestion" src={question.img} alt="imgQuest" />
       </div>
-      <form className="answerForm" action="/api/question" method="post">
-        <input type="text" name="answers" placeholder="answer" required />
-        <button type="submit">ОК</button>
-      </form>
       <div className="dopMessage" />
-      <a href={`/questions/${question.themeId}/${question.id + 1}`}>Далее</a>
+      <div className="footer">
+        <form className="answerForm" action="/api/question" method="post">
+          <input type="text" name="answers" placeholder="answer" required />
+          <button type="submit">ОК</button>
+        </form>
+        <a className="buttonDalee" href={`/questions/${question.themeId}/${question.id + 1}`}>Далее</a>
+      </div>
     </div>
+
   );
 }
 
